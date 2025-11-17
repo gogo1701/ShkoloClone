4e52921dbdc8486f84a9e860df1242243f69a79f
BorkoAXT <borkoaxtyt@gmail.com>
2025-11-17 13:45:18 +0200

    Add Student model class
    Introduced the Student class in the Models namespace with private fields for id, first name, last name, email, phone, and address. This sets up the basic structure for representing student data in the application.

---
+++ ShkoloClone/ShkoloClone/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ShkoloClone.Models
+{
+    public class Student
+    {
+        //[JsonPropertyName()] before every field. It tells the JsonSerializer what name the field should have in the JSON file.
+        //[JsonConverter(typeof(JsonStringEnumConverter))] before fields which use an enum type. Basically just tells the JsonSerializer to convert the enum type to a string.
+        
+        private int _id;
+        
+        private string _firstName;
+        
+        private string _lastName;
+        
+        private string _email;
+        
+        private string _phone;
+       
+        private string _address;
+
+
+
+
+    }
+}
